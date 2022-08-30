a55d0abe9262d17fe36d91fe605b429ddc08d614
luizasamorim <luizassamorim@gmail.com>
2022-08-30 19:11:49 -0300

    atualizando o repo

---
+++ src/editarVacina/editarVacina.js
@@ -0,0 +1,29 @@
+function excluirVacina() {
+    
+}
+
+function editarVacina() {
+    
+}
+
+/*
+function preview(){
+    alert('call')
+    var imagem = document.getElementById('comprovante').files
+    var preview = document.getElementById('preview')
+    
+    var reader = new FileReader();
+    
+    reader.onloadend = function () {
+        preview.src = reader.result;
+    }
+    
+    if(imagem){
+        alert('aaaa')
+        reader.readAsDataURL(imagem);
+    }else{
+        alert('bbbbb')
+        preview.src = "";
+    }
+}
+*/
--- src/home/home.js
+++ src/home/home.js
@@ -1,3 +1,9 @@
-function criarConta() {
-    window.location = '../criarConta/criarConta.html'
+function novaVacina() {
+    window.location = '../novaVacina/novaVacina.html'
+}
+
+function editarVacina(index) {
+    // card = document.getElementsByClassName('card').item(index)
+
+    window.location = '../editarVacina/editarVacina.html'    
 }
